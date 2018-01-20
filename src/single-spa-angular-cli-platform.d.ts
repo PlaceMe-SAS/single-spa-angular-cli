922a929f5975c465303967762830bb7bd91382f5
Robin Coma Delperier <robin@placeme.io>
2018-01-20 16:22:09 +0100

    feat(TS): compilation ok

--- src/single-spa-angular-cli-platform.d.ts
+++
@@ -1,8 +0,0 @@
-import { Observable } from 'rxjs/Observable';
-export declare class SingleSpaAngularCliPlatform {
-    appName: string;
-    mount(appName: string): Observable<Function>;
-    unmount(module: any): void;
-    private isSingleSpaApp();
-}
-export declare const singleSpaAngularCliPlatform: SingleSpaAngularCliPlatform;
